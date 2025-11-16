4bdee6fdd1bcd0160b1aae9b8d381b5f81f6316f
riderproe <riderleomessi@gmail.com>
2025-11-16 13:59:38 +0000

    Update index.ts

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,8 +1,8 @@
 export const brand = {
   name: 'GamingCoinHub Agent',
   emoji: '🎰',
-    telegram: 'https://t.me/yourtelegram',
-    signal: 'https://signal.group/#yourgroup',
+    telegram: 'https://t.me/royalcasinox',
+    signal: 'https://signal.group/#judyosborneX.17',
     x: 'https://x.com/yourhandle',
     facebook: 'https://facebook.com/yourpage',
     email: 'info@gamingcoinhub.com'
